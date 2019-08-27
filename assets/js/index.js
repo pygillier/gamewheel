feea4d9329743625678348401f57d319b0f19334
Pierre-Yves Gillier <bonjour@pygillier.me>
2019-08-27 15:34:50 +0200

    Various
    Signed-off-by: Pierre-Yves Gillier <pierre-yves.gillier@fr.clara.net>

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,3 +3,14 @@ import '@fortawesome/fontawesome-free/js/solid'
 import '@fortawesome/fontawesome-free/js/regular'
 import '@fortawesome/fontawesome-free/js/brands'
 import './main.scss'
+
+
+// handle delete button in notifications
+document.addEventListener('DOMContentLoaded', () => {
+  (document.querySelectorAll('.notification .delete') || []).forEach(($delete) => {
+    $notification = $delete.parentNode;
+    $delete.addEventListener('click', () => {
+      $notification.parentNode.removeChild($notification);
+    });
+  });
+});
--- home/urls.py
+++ home/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from home.views import HomeView
+from home import views
 
 app_name = 'home'
 urlpatterns = [
-    path('', HomeView.as_view(), name='index'),
+    path('', views.HomeView.as_view(), name='index'),
+    path('play/', views.PlayView.as_view(), name='play')
 ]
